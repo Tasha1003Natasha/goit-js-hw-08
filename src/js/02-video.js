b1b1d513547102bfe856a82ffdbe2ea72026a101
Tasha1003Natasha <0955712824n@gmail.com>
2022-06-20 22:52:53 +0300

    Початок 2 завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(function (data) {
+//   console.log('played the video!');
+//   console.log(data);
+
+  localStorage.setItem(`videoplayer-current-time`, data.seconds);
+//   console.log(videoplayer-current-time);
+
+}, 1000));
+
+
+// player.setCurrentTime()
+
+
+
+
+
+
+// function iframeAll ()  {
+   
+// }  
+
+// player.on(`play`, iframeAll) 
+// player.off(`play`, iframeAll) 
