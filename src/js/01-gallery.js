17e5b6388fe0834ede216f6e51b60407dad50821
Tasha1003Natasha <0955712824n@gmail.com>
2022-06-20 17:50:39 +0300

    Задача № 1 готово

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Підключення бібліотеки SimpleLightbox
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+
+// Посилання на gallery (отримали доступ до gallery з HTML)
+const galleryContainer = document.querySelector(".gallery");
+
+
+// Зміна, яка містить посилання на функцію створення зображень
+const galleryMarkup = createGalleryItems(galleryItems);
+
+// Підключення галереї після div class="gallery" в розмітці
+galleryContainer.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+
+// Функція створення галереї зображень
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" loading="lazy" alt="${description}" />
+</a>
+`;
+    })
+    .join("");
+}
+
+
+// Модальне вікно +  Функція подія клік
+
+  let gallery = new SimpleLightbox(".gallery a",  { captionsData:`alt`, captionDelay:`250`});
+  
