77b969ffad7b6e81b95eabe4397b030db4bc4732
Tasha1003Natasha <0955712824n@gmail.com>
2022-06-21 14:32:15 +0300

    Задача 2 без перевірки

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,22 +3,27 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const VIDEOPLAYER_TIME = `videoplayer-current-time`;
+
 
 player.on('timeupdate', throttle(function (data) {
-//   console.log('played the video!');
-//   console.log(data);
+  // console.log(data);
 
-  localStorage.setItem(`videoplayer-current-time`, data.seconds);
-//   console.log(videoplayer-current-time);
+  // JSON.stringify(localStorage.setItem(VIDEOPLAYER_TIME, data.seconds));
+  // console.log(VIDEOPLAYER_TIME);
+
+ localStorage.setItem(VIDEOPLAYER_TIME, data.seconds);
+  console.log(VIDEOPLAYER_TIME);
 
 }, 1000));
 
 
-// player.setCurrentTime()
+// const setTime = player.setCurrentTime(JSON.parse(localStorage.getItem(VIDEOPLAYER_TIME)));
+// console.log(setTime);
 
 
-
-
+const setTime =  player.setCurrentTime(localStorage.getItem(VIDEOPLAYER_TIME));
+console.log(setTime);
 
 
 // function iframeAll ()  {
